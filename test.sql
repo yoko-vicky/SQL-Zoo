d64a1c6a36afa95a92bcaabd5742161b0b8faa57
yocosaka <sakayoco55@gmail.com>
2020-11-24 10:43:44 +0900

    Add SELECT within SELECT Tutorial

--- test.sql
+++ test.sql
@@ -104,3 +104,27 @@ SELECT name FROM world
      (SELECT population FROM world
       WHERE name='Russia')
 -- 2
+SELECT name FROM world
+WHERE continent = 'Europe'
+AND gdp/population > (SELECT gdp/population FROM world WHERE name = 'United Kingdom')
+-- 3
+SELECT name, continent FROM world
+WHERE continent IN(SELECT continent FROM world WHERE name IN('Argentina', 'Australia')) ORDER BY name
+-- 4
+SELECT name, population FROM world
+WHERE population > (SELECT population FROM world WHERE name = 'Canada')
+AND population < (SELECT population FROM world WHERE name = 'Poland')
+-- 5
+SELECT name, CONCAT(ROUND(100*population/(SELECT population FROM world WHERE name = 'Germany')) , '%')
+FROM world
+WHERE continent = 'Europe'
+-- 6
+SELECT name FROM world
+WHERE gdp > (SELECT gdp FROM world WHERE continent ='Europe' AND gdp IS NOT NULL ORDER BY gdp DESC LIMIT 1)
+-- 7
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area > 0)
+-- 8
